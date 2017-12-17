0d5d94c4811adbefba45aa2a927e60f80462fa81
edgar <spickerman.edgar@gmail.com>
2017-12-16 22:03:47 -0800

    added userSchema properties added an authenticate method to the userSchema added a pre method to hash passwords before saving

--- src/functions/errors.js
+++ src/functions/errors.js
@@ -1,6 +1,7 @@
 'use strict';
 //load modules
 
+//404 Errors
 const pageNotFound = () => {
   let err = new Error();
   err.name = 'Page Not Found';
@@ -9,5 +10,23 @@ const pageNotFound = () => {
   return err;
 }
 
+//401 Errors
+const noUserFound = () => {
+  let err = new Error();
+  err.name = 'No User Found';
+  err.message = 'Sorry, no user was found with that username';
+  err.status = 401;
+  return err;
+}
+const invalidPass = () => {
+  let err = new Error();
+  err.name = 'Invalid/incorrect Password';
+  err.message = 'Sorry, you entered an incorrect password';
+  err.status = 401;
+  return err;
+}
+
 //Error exports
 module.exports.pageNotFound = pageNotFound;
+module.exports.noUserFound = noUserFound;
+module.exports.invalidPass = invalidPass;
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -2,10 +2,50 @@
 //load modules
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const { noUserFound, invalidPass } = require('../functions/errors');
 
 //declare review schema
 const userSchema = new Schema({
+  fullName: {
+    type: String,
+    required: [true, 'Full Name is Required']
+  },
+  emailAddress: {
+    type: String,
+    unique:true,
+    required: [true, 'Email Address is Required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is Required']
+  },
 })
+
+//hasing passwords before saving individual docs
+userSchema.pre('save', function (next) {
+  let user = this;
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+//authentication method of each model instance
+userSchema.statics.authenticate = function (email, pass, callback) {
+  this.findOne({ emailAddress: email })
+    .exec((err, instance) => {
+      if (err) return callback(err);
+      if (!instance) return callback(noUserFound());
+      bcrypt.compare(pass, instance.password, (err, res) => {
+        if (err) return callback(err);
+        if (!res) return callback(invalidPass());
+        callback(null, instance);
+      })
+    })
+}
+
 
 //export Schema
 module.exports = userSchema;
