a4cd62edcd8c7c8fb55542f30ee7a93daeda0d54
edgar <spickerman.edgar@gmail.com>
2017-12-17 00:34:49 -0800

    separated out the seeder completely to its own module

--- src/functions/connection.js
+++ src/functions/connection.js
@@ -1,17 +1,9 @@
 'use strict';
 //load modules
 const mongoose = require('mongoose');
-const seeder = require('mongoose-seeder');
-const data = require('../data/data.json');
 
 //overwriting deprecated mongoose promise
 mongoose.Promise = require('bluebird');
-
-//Schemas
-const reviewSchema = require('../schemas/reviewSchema');
-const stepsSchema = require('../schemas/stepsSchema');
-const courseSchema = require('../schemas/courseSchema');
-const userSchema = require('../schemas/userSchema');
 
 //functions
 const connectDatabase = (port, dbName, opts) => {
@@ -32,18 +24,5 @@ const connectDatabase = (port, dbName, opts) => {
   });
 }
 
-const seedDB = () => {
-  return new Promise((resolve, reject) => {
-    const review = mongoose.model('Review', reviewSchema);
-    const step = mongoose.model('Step', stepsSchema);
-    const course = mongoose.model('Course', courseSchema);
-    const user = mongoose.model('User', userSchema);
-    seeder.seed(data)
-      .then(dbData=>resolve('Database Seed Success'))
-      .catch(err => reject('Database Seed Error' + err))
-  });
-}
-
 //export
 module.exports.connectDatabase = connectDatabase;
-module.exports.seedDB = seedDB;
--- src/seed.js
+++ src/seed.js
@@ -1,29 +1,40 @@
+//connects to a database named `dbName`
+//if there is an error it will log err to console
+//if success it logs a success message
+//after the success msg it then attempts to seed the database
+//if there is an error it will log seed error and then close the db connection
+//if there is no error then it will log seed success then close the db connection
+
 'use strict';
 //load modules
 const mongoose = require('mongoose');
-const { connectDatabase, seedDB } = require('./functions/connection');
+const seeder = require('mongoose-seeder');
+const { connectDatabase } = require('./functions/connection');
+const data = require('./data/data.json');
 
 //overwriting depreciated mongooose promise
 mongoose.Promise = require('bluebird');
 
-/*
-connects to a database named `dbName`
-  if there is an error it will log err to console
-  if success it logs a success message
-    after the success msg it then attempts to seed the database
-      if there is an error it will log seed error and then close the db connection
-      if there is no error then it will log seed success then close the db connection
-*/
+//Schemas and models
+const reviewSchema = require('./schemas/reviewSchema');
+const stepsSchema = require('./schemas/stepsSchema');
+const courseSchema = require('./schemas/courseSchema');
+const userSchema = require('./schemas/userSchema');
+const review = mongoose.model('Review', reviewSchema);
+const step = mongoose.model('Step', stepsSchema);
+const course = mongoose.model('Course', courseSchema);
+const user = mongoose.model('User', userSchema);
+
 let dbName = 'courseRating';
 
 connectDatabase(27017, dbName)
   .then(success => console.log(success))
   .then(() => {
-    seedDB()
-      .then(success => console.log(success))
+    seeder.seed(data)
+      .then(dbData => console.log('Database Seed Success'))
       .then(() => mongoose.connection.close())
       .catch(err => {
-        console.error(err);
+        console.error('Database Seed Error', err);
         mongoose.connection.close();
       })
   }).catch(err => console.error(err))
